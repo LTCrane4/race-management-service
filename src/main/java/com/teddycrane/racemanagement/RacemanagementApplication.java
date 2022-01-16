ff9742da25885b6390d6ee58771bffc0afe18515
Teddy Crane <teddycrane@gmail.com>
2022-01-16 06:29:58 -0800

    update with impl files

---
+++ src/main/java/com/teddycrane/racemanagement/RacemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.teddycrane.racemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teddycrane/racemanagement/RacemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddycrane.racemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
