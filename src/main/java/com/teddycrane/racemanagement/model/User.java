45254ee972da46edcba6ca34f13844e249c144df
Teddy Crane <teddycrane@gmail.com>
2022-01-16 06:51:28 -0800

    Add code coverage jacoco

--- src/main/java/com/teddycrane/racemanagement/model/User.java
+++ src/main/java/com/teddycrane/racemanagement/model/User.java
@@ -1,7 +1,6 @@
 package com.teddycrane.racemanagement.model;
 
 import java.util.UUID;
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
