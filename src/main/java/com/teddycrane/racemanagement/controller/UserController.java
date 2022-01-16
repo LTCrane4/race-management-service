6e41a95bf42fa9175e6da2d5fcfe4fc51bcf242c
Teddy Crane <teddycrane@gmail.com>
2022-01-16 07:09:30 -0800

    remove class files and set up circle

--- src/main/java/com/teddycrane/racemanagement/controller/UserController.java
+++ src/main/java/com/teddycrane/racemanagement/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.teddycrane.racemanagement.controller;
 
-import org.springframework.stereotype.Controller;
+import com.teddycrane.racemanagement.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "/user")
+@RestController
 public class UserController extends BaseController {
-  public UserController() {
-    super();
-  }
+  public UserController() { super(); }
 
+  @GetMapping("/{id}")
+  public User getUser(@PathVariable String id) {
+    logger.trace("getUser called");
+
+    return new User();
+  }
 }
--- src/main/java/com/teddycrane/racemanagement/model/User.java
+++ src/main/java/com/teddycrane/racemanagement/model/User.java
@@ -1,11 +1,13 @@
 package com.teddycrane.racemanagement.model;
 
+import com.teddycrane.racemanagement.enums.UserType;
 import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.hibernate.annotations.Type;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class User {
@@ -14,9 +16,29 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private final UUID id;
 
+  @NonNull private String firstName, lastName, email, username, password;
+
+  @NonNull private UserType userType;
+
   public User() { this.id = UUID.randomUUID(); }
 
-  private User(UUID id) { this.id = id; }
+  // private User(UUID id) { this.id = id; }
+
+  public User(String firstName, String lastName, String username, String email,
+              String password) {
+    this();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email; // TODO update email validation
+    this.username = username;
+    this.password = password;
+  }
+
+  public User(String firstName, String lastName, String username, String email,
+              String password, UserType userType) {
+    this(firstName, lastName, username, email, password);
+    this.userType = userType;
+  }
 
   private boolean equals(User other) { return this.id.equals(other.id); }
 
