355e70c6f90e63f4eb79ee0c1f7ec28cf7168cef
Teddy Crane <teddy.crane@slalom.com>
2022-01-17 11:18:44 -0800

    add deps

---
+++ src/main/java/com/teddycrane/racemanagement/util/TokenManager.java
@@ -0,0 +1,47 @@
+package com.teddycrane.racemanagement.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenManager implements Serializable {
+  private static final long serialVersionUID = 7008375124389347049L;
+
+  @Value("${secret}") private String jwtSecret;
+
+  private static final long TOKEN_VALIDITY = 10 * 60 * 60;
+
+  public String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(
+            new Date(System.currentTimeMillis() + TOKEN_VALIDITY + 1000))
+        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+        .compact();
+  }
+
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    String username = this.getUsernameFromToken(token);
+    Claims claims =
+        Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+    Boolean isTokenExpired = claims.getExpiration().before(new Date());
+    return (username.equals(userDetails.getUsername())) && isTokenExpired;
+  }
+
+  public String getUsernameFromToken(String token) {
+    final Claims claims =
+        Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+    return claims.getSubject();
+  }
+}
