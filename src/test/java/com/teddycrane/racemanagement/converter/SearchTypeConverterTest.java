a69a5b888c89b0a9be83b9cab9df5eb7de892502
Teddy Crane <teddy.crane@slalom.com>
2022-01-20 14:51:41 -0800

    update tests

---
+++ src/test/java/com/teddycrane/racemanagement/converter/SearchTypeConverterTest.java
@@ -0,0 +1,34 @@
+package com.teddycrane.racemanagement.converter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.teddycrane.racemanagement.config.converter.SearchTypeConverter;
+import com.teddycrane.racemanagement.enums.SearchType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SearchTypeConverterTest {
+
+  private SearchTypeConverter converter;
+
+  @BeforeEach
+  void setUp() {
+    this.converter = new SearchTypeConverter();
+  }
+
+  @Test
+  void shouldConvert() {
+    assertEquals(
+        SearchType.TYPE,
+        this.converter.convert("type"),
+        "The converter should convert valid enum values");
+  }
+
+  @Test
+  void shouldThrow() {
+    assertNull(
+        this.converter.convert("bad value"),
+        "The converter should return null if the input is not a valid enum value");
+  }
+}
