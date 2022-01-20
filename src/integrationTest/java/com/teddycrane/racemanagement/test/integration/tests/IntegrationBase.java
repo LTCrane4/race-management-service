b161802927c19f61258d1bc45a47aad8e2399500
Teddy Crane <78568061+teddycrane@users.noreply.github.com>
2022-01-20 10:30:31 -0800

    Feat/add integration tests (#10)
    * test commit
    
    * add integration tests
    
    * add paren
    
    * add integration tests

---
+++ src/integrationTest/java/com/teddycrane/racemanagement/test/integration/tests/IntegrationBase.java
@@ -0,0 +1,76 @@
+package com.teddycrane.racemanagement.test.integration.tests;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import com.teddycrane.racemanagement.repositories.UserRepository;
+import com.teddycrane.racemanagement.security.util.TokenManager;
+import com.teddycrane.racemanagement.services.AuthenticationService;
+import com.teddycrane.racemanagement.test.integration.utils.JwtTokenProviderMock;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+@WithMockUser("testuser")
+public class IntegrationBase {
+
+  @Container
+  public static final MySQLContainer<?> CONTAINER =
+      new MySQLContainer<>("mysql:8.0.20")
+          .withDatabaseName("test")
+          .withUsername("tester")
+          .withPassword("password")
+          .waitingFor(Wait.defaultWaitStrategy());
+
+  protected static final String AUTHORIZATION_HEADER = "Authorization";
+  protected static final String BEARER = "Bearer";
+  protected static final String USER_TOKEN = String.format(
+      "%s %s", BEARER, JwtTokenProviderMock.generateMockToken("testuser"));
+
+  protected Logger logger = LogManager.getLogger(this.getClass());
+
+  @Autowired protected UserRepository userRepository;
+
+  @Autowired protected MockMvc mockMvc;
+
+  // required to get tests running because it won't construct otherwise
+  @MockBean private AuthenticationService authService;
+
+  @MockBean private TokenManager tokenManager;
+
+  @BeforeAll
+  public static void setUpDatabase() {
+    CONTAINER.start();
+  }
+
+  private void setUpMockTokenManager() {
+    when(this.tokenManager.validateToken(anyString(), any(UserDetails.class)))
+        .thenReturn(true);
+    // hard code token manager to return the test user
+    when(this.tokenManager.getUsernameFromToken(anyString()))
+        .thenReturn("testuser");
+  }
+
+  @BeforeEach
+  void setUp() {
+    setUpMockTokenManager();
+  }
+}
---
+++ src/integrationTest/java/com/teddycrane/racemanagement/test/integration/tests/users/UserBase.java
@@ -0,0 +1,8 @@
+package com.teddycrane.racemanagement.test.integration.tests.users;
+
+import com.teddycrane.racemanagement.test.integration.tests.IntegrationBase;
+
+abstract class UserBase extends IntegrationBase {
+  protected static final String USER_ID =
+      "bf68eea7-25a6-4dbf-867b-0829e308bbdd";
+}
