07312a4132686d987c17ceca3e5a470472dd111a
Teddy Crane <teddycrane@gmail.com>
2022-01-31 06:51:39 -0800

    add missed files

--- src/main/java/com/teddycrane/racemanagement/services/RaceService.java
+++ src/main/java/com/teddycrane/racemanagement/services/RaceService.java
@@ -1,6 +1,10 @@
 package com.teddycrane.racemanagement.services;
 
+import com.teddycrane.racemanagement.model.race.Race;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface RaceService {}
+public interface RaceService {
+  List<Race> getAllRaces();
+}
--- src/test/java/com/teddycrane/racemanagement/controller/RaceControllerTest.java
+++ src/test/java/com/teddycrane/racemanagement/controller/RaceControllerTest.java
@@ -1,7 +1,17 @@
 package com.teddycrane.racemanagement.controller;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import com.teddycrane.racemanagement.helper.TestResourceGenerator;
+import com.teddycrane.racemanagement.model.race.Race;
 import com.teddycrane.racemanagement.services.RaceService;
-// import org.junit.jupiter.api.BeforeEach;
+import java.util.Collection;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,10 +21,21 @@ class RaceControllerTest {
 
   @Mock private RaceService raceService;
 
-  // private RaceApi raceController;
+  private RaceApi raceController;
+  private Collection<Race> races = TestResourceGenerator.generateRaceList();
 
-  // @BeforeEach
-  // void setUp() {
-  // this.raceController = new RaceController(this.raceService);
-  // }
+  @BeforeEach
+  void setUp() {
+    this.raceController = new RaceController(this.raceService);
+  }
+
+  @Test
+  @DisplayName("Should get all races")
+  void shouldGetAllRaces() {
+    when(this.raceService.getAllRaces()).thenReturn((List<Race>) races);
+
+    var result = this.raceController.getAllRaces();
+
+    assertAll(() -> assertNotNull(result, "The result should not be null"));
+  }
 }
