ac039c597382a6732d8a5c68f92f2f16baca82b7
Teddy Crane <teddy.crane@slalom.com>
2022-02-04 08:54:12 -0800

    testing ci parallelism

---
+++ src/integrationTest/java/com/teddycrane/racemanagement/test/integration/tests/racer/CreateRacer.java
@@ -0,0 +1,35 @@
+package com.teddycrane.racemanagement.test.integration.tests.racer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.teddycrane.racemanagement.enums.Category;
+import com.teddycrane.racemanagement.model.racer.request.CreateRacerRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+class CreateRacer extends RacerBase {
+
+  @Test
+  void shouldCreateRacer() throws Exception {
+    var body =
+        CreateRacerRequest.builder()
+            .firstName("Test")
+            .lastName("Racer")
+            .category(Category.CAT1.toString())
+            .build();
+
+    this.mockMvc
+        .perform(
+            post(this.RACER_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION_HEADER, USER_TOKEN)
+                .content(this.mapper.writeValueAsString(body)))
+        .andExpect(status().isOk())
+        .andExpectAll(
+            jsonPath("$").exists(),
+            jsonPath("$.firstName").value("Test"),
+            jsonPath("$.lastName").value("Racer"));
+  }
+}
